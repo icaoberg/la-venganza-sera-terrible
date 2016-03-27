71d18a24fbd9c1626d2fe07738666ea8eda6f2d5
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-03-27 15:48:16 -0400

    Updated main script

--- get_podcasts.sh
+++ get_podcasts.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-for I in {1..100}
+if [ $# -eq 0 ]; then
+		MAX_NUMBER_OF_PAGES=5
+else
+		MAX_NUMBER_OF_PAGES=$1
+fi
+echo "Setting maximum number of pages to "$MAX_NUMBER_OF_PAGES
+
+for ((I=0; I<=MAX_NUMBER_OF_PAGES; I++))
 do
-	wget -nc  --output-document=temp.html http://venganzasdelpasado.com.ar/posts/page/$I
-	grep "mp3" *.html | grep "http://venganzasdelpasado.com.ar" | grep "audio/mpeg" | cut -d"'" -f 2 | xargs wget -nc 
-	rm -fv *.html
+	wget -nc --quiet --output-document=temp.html http://venganzasdelpasado.com.ar/posts/page/$I
+	echo "Attempting to download podcasts from page number "$I
+	grep "mp3" *.html | grep "http://venganzasdelpasado.com.ar" | grep "audio/mpeg" | cut -d"'" -f 2 | xargs wget -nc
+	rm -f *.html
 done
